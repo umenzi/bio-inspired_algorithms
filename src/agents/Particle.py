f38828e369980e27aa4b575bb59e1f0f588ddd0d
Juan Tarazona <jjtaracan@gmail.com>
2024-03-28 19:45:21 +0100

    working pso with levy flights and adaptive coefficients

--- src/agents/Particle.py
+++ src/agents/Particle.py
@@ -16,8 +16,8 @@ class Particle(Agent):
     """
 
     def __init__(self, environment: Environment, path_specification: PathSpecification,
-                 convergence_iter: int, trail: float, velocity_x: int, velocity_y: int, step_size: int = 1,
-                 inertia_weight: int = 1):
+                 convergence_iter: int, trail: float, velocity_x: float, velocity_y: float, step_size: int = 1,
+                 inertia_weight: float = 1.0):
         super().__init__(environment, path_specification, step_size)
 
         self.personal_best_pos = self.current_position  # initially personal best is start
@@ -31,48 +31,39 @@ class Particle(Agent):
         self.velocity_y = velocity_y
         self.w = inertia_weight
 
-    def update_particle(self, global_best_pos: Coordinate, personal_best_pos: Coordinate):
-        # need to do the update with integers
-        # random variables, need to be ints
-        r1: int = self.rand.randint(0, 2)
-        r2: int = self.rand.randint(0, 2)
-        new_vel_x: int = (self.w * self.velocity_x + r1 * (personal_best_pos.x - self.current_position.x)
-                          + r2 * (global_best_pos.x - self.current_position.x))
-        new_vel_y: int = (self.w * self.velocity_y + r1 * (personal_best_pos.y - self.current_position.y)
-                          + r2 * (global_best_pos.y - self.current_position.y))
-        # clamp the velocities into the range [-1, 1]
-        new_vel_x = max(-1, min(1, new_vel_x))
-        new_vel_y = max(-1, min(1, new_vel_y))
-        # update the position, and the personal best position and return the position to update global best
+    def update_particle(self, global_best_pos: Coordinate, personal_best_pos: Coordinate, c1: float, c2: float,
+                        curr_iter: int, iter_max: int):
+        # random coefficients r1, r2
+        r1: float = self.rand.uniform(0, 1)
+        r2: float = self.rand.uniform(0, 1)
+        # evaluate the inertia weight
+        if c1 >= c2:
+            self.w = math.exp(((c1 - c2) / (c1 + c2)) * (curr_iter / iter_max)) - 0.2
+        else:
+            self.w = (1 / 3) * (c1 / c2) * (iter_max / curr_iter)
+        # update the velocities according to pso strategy(c1-c2)/(c1+c2)
+        new_vel_x: float = (self.w * self.velocity_x + c1 * r1 * (personal_best_pos.x - self.current_position.x)
+                            + c2 * r2 * (global_best_pos.x - self.current_position.x))
+        new_vel_y: float = (self.w * self.velocity_y + c1 * r1 * (personal_best_pos.y - self.current_position.y)
+                            + c2 * r2 * (global_best_pos.y - self.current_position.y))
+        # clamp new velocity to [-2, 2]
+        new_vel_x = np.clip(new_vel_x, -2, 2)
+        new_vel_y = np.clip(new_vel_y, -2, 2)
+        # record the new position
         new_pos: Coordinate = Coordinate(self.current_position.x + new_vel_x, self.current_position.y + new_vel_y)
-        # update the velocities and personal best and return the new position if it is not out of bounds
+        # update the velocities and current position if it is not out of bounds
+        # meaning if not obstacle or out of search space
         if not self.position_out_of_bounds(new_pos):
             self.velocity_x = new_vel_x
             self.velocity_y = new_vel_y
             self.current_position = new_pos
-            if self.evaluate_fitness(new_pos) < self.evaluate_fitness(personal_best_pos):
-                self.personal_best_pos = new_pos
 
-        return self.personal_best_pos  # return the personal best to compete for global best
-
-    def evaluate_fitness(self, pos: Coordinate):
-        # minimize the distance to the goal but maximize the distance to the nearest obstacle
-        distance_to_goal = math.sqrt((pos.x - self.end.x) ** 2 + (pos.y - self.end.y) ** 2)
-        distance_to_obstacle = self.environment.distance_to_closest_obstacle(pos)
-
-        # for now, we assign weight 1 to the goal distance and 0 to the obstacle distance
-        # effectively only the goal distance is considered
-        weight_goal: float = 1.0
-        weight_obstacle: float = 300.0
-
-        # since we need to maximize one and minimize the other, we invert distance to obstacle, so we can minimize it
-
-        return weight_goal * distance_to_goal + weight_obstacle * (1 / distance_to_obstacle)
+        return self.current_position  # return the current position
 
     def position_out_of_bounds(self, pos: Coordinate):
         # check if within bounds OR colliding with an obstacle
-        xout: bool = not pos.x_between(0, self.environment.width)
-        yout: bool = not pos.y_between(0, self.environment.height)
+        xout: bool = not (0 <= pos.x <= self.environment.width - 1)
+        yout: bool = not (0 <= pos.y <= self.environment.height - 1)
         obs: bool = self.environment.distance_to_closest_obstacle(pos) < 0
 
         return xout or yout or obs
--- src/algorithms/ParticleSwarmOptimization.py
+++ src/algorithms/ParticleSwarmOptimization.py
@@ -1,40 +1,117 @@
+import math
 import random
+import numpy as np
 
 from helpers.Route import Route
+from helpers.Coordinate import Coordinate
+from helpers.PathSpecification import PathSpecification
+from environments.Environment import Environment
 from agents.Particle import Particle
 
 
 class ParticleSwarmOptimization:
-    def __init__(self, environment, path_specification, num_particles, convergence_iter, trail, step_size,
-                 inertia_weight):
+    def __init__(self, environment: Environment, path_specification: PathSpecification, num_particles: int,
+                 convergence_iter: int, trail: float, step_size: int, inertia_weight: float, max_iter: int = 100):
         self.environment = environment
         self.path_specification = path_specification
         self.num_particles = num_particles
         self.particles = []
         self.global_best_pos = path_specification.start
+        self.levy_best = path_specification.start
         self.route = Route(path_specification.start)
+        self.max_iter = max_iter
 
-        # Initialize particles
+        # Initialize particles with random velocities
         for _ in range(num_particles):
-            velocity_x = random.randint(-1, 1)
-            velocity_y = random.randint(-1, 1)
+            velocity_x = random.uniform(-1, 1)
+            velocity_y = random.uniform(-1, 1)
             particle = Particle(environment, path_specification, convergence_iter, trail, velocity_x, velocity_y,
                                 step_size, inertia_weight)
             self.particles.append(particle)
 
     def run(self):
-        for _ in range(100):
-            # two loops over particles, first find the global best then the particles
+        const_count: int = 0
+        for _ in range(self.max_iter):
+            # get the coefficients c1 and c2 based on iteration
+            c1: float = math.cos((math.pi / 2) * (_ / self.max_iter)) * math.cos(math.pi * (_ / self.max_iter)) + 1.5
+            c2: float = (math.sin((math.pi / 2) * (_ / self.max_iter)) * math.sin(math.pi * ((_ / self.max_iter) + 1.5))
+                         + 1.5)
+            # update the speeds and positions of particles
             for particle in self.particles:
-                if ((particle.evaluate_fitness(particle.current_position) <
-                     particle.evaluate_fitness(self.global_best_pos))
-                        and not particle.position_out_of_bounds(particle.current_position)):
-                    self.global_best_pos = particle.current_position
+                particle.update_particle(self.global_best_pos, particle.personal_best_pos, c1, c2, _, self.max_iter)
 
+            # check if the particles have fallen into poor areas and get them out with Levy flight
+            # TODO: implement Levy flight
+            # the particles are judged to have fallen into a poor area if they do not change in more than 10 iters
+            if self.levy_best == self.global_best_pos:
+                const_count += 1
+                if const_count > 10:
+                    # do Lévy flight
+                    for particle in self.particles:
+                        levy_steps = self.levy_flight(beta=1.5, size=2)
+                        levy_vel_x = levy_steps[0]
+                        levy_vel_y = levy_steps[1]
+                        levy_pos = Coordinate(particle.current_position.x + levy_vel_x,
+                                              particle.current_position.y + levy_vel_y)
+                        # take the direction that is allowed
+                        if self.environment.distance_to_closest_obstacle(levy_pos) > 0:
+                            if 0 <= levy_pos.x <= self.environment.width - 1:
+                                particle.current_position.x = levy_pos.x
+                                particle.velocity_x = levy_vel_x
+                            if 0 <= levy_pos.y <= self.environment.height - 1:
+                                particle.current_position.y = levy_pos.y
+                                particle.velocity_y = levy_vel_y
+            else:
+                # reset count and levy best
+                const_count = 0
+                self.levy_best = self.global_best_pos
+
+            # calculate particle fitness values, update personal bests and global best
             for particle in self.particles:
-                particle.update_particle(self.global_best_pos, particle.personal_best_pos)
+                fitness: float = self.evaluate_fitness(particle.current_position)
+                fitness_pb: float = self.evaluate_fitness(particle.personal_best_pos)
+                fitness_gb: float = self.evaluate_fitness(self.global_best_pos)
+                if fitness < fitness_pb:
+                    particle.personal_best_pos = particle.current_position
+                if fitness < fitness_gb:
+                    self.global_best_pos = particle.current_position
 
             # Add global best to route
             self.route.add(self.global_best_pos)
+            print(self.global_best_pos)
+
+            # check termination conditions (global best at end position)
+            # we want to see if the global best is within 0.5 (safe margin) of the end position set the global best to
+            # end, append end to route and stop
+            if self.global_best_pos.x_between(self.environment.end.x - 0.5, self.environment.end.x + 0.5) and \
+                    self.global_best_pos.y_between(self.environment.end.y - 0.5, self.environment.end.y + 0.5):
+                self.route.add(self.environment.end)
+                return self.route
 
         return self.route
+
+    def evaluate_fitness(self, pos: Coordinate):
+        # minimize the distance to the goal but maximize the distance to the nearest obstacle
+        distance_to_goal = math.sqrt((pos.x - self.environment.end.x) ** 2 + (pos.y - self.environment.end.y) ** 2)
+        distance_to_obstacle = self.environment.distance_to_closest_obstacle(pos)
+
+        # for now, we assign weight 1 to the goal distance and 0 to the obstacle distance
+        # effectively only the goal distance is considered
+        weight_goal: float = 1.0
+        weight_obstacle: float = 0.0
+
+        # since we need to maximize one and minimize the other, we invert distance to obstacle, so we can minimize it
+
+        return weight_goal * distance_to_goal + weight_obstacle * (1 / distance_to_obstacle)
+
+    def levy_flight(self, beta: float, size: int):
+        # Draw samples from a uniform distribution
+        u = np.random.uniform(0.01, 1, size=size)
+
+        # Calculate the corresponding step lengths
+        steps = u ** (-1 / beta)
+
+        # make sure the steps are between -2 and 2 (obstacle radius)
+        steps = np.clip(steps, -2, 2)
+
+        return steps
